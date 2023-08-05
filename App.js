3944d277aa37de2b9266f01e5fb7e0800f392eec
zeeshan710 <mirzazeeshan710@gmail.com>
2023-08-05 20:48:43 +1000

    initial setup

---
+++ App.js
@@ -0,0 +1,11 @@
+const heading = React.createElement(
+  "h1",
+  {
+    id: "heading",
+  },
+  "Hello World from Zee in React"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
